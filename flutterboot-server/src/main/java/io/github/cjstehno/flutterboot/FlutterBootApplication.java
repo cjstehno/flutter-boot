5f663a612f72960f49eafe2a1fad5a3deaa71889
Christopher J. Stehno <chris.stehno@gmail.com>
2022-11-14 04:42:47 -0600

    Initial commit of server - still has example content

---
+++ flutterboot-server/src/main/java/io/github/cjstehno/flutterboot/FlutterBootApplication.java
@@ -0,0 +1,13 @@
+package io.github.cjstehno.flutterboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlutterBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlutterBootApplication.class, args);
+	}
+
+}
